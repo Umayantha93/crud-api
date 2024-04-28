cb7947067d40d914de2124c9510fdc45e177fce3
Umayantha93 <umayantha93@gmail.com>
2024-04-28 11:14:12 +0530

    create function is completed

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
 {
@@ -15,4 +16,20 @@ class PersonController extends Controller
         return $persons;
     }
 
+    public function insert(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'age' => 'required|integer',
+            'phone_number' => 'required'
+        ]);
+
+        $item = Person::create($validatedData);
+
+        return response()->json($item, 201);
+    }
+
+
 }
+
+
